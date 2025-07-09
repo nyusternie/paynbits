2e606995ab5b1154eee2133ae3fe43ce554dd2fa
Shomari <nyusternie@sdot.io>
2025-07-09 16:13:57 -0400

    Enable mapbox map view.

---
+++ src/components/MapView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div ref="mapContainer" class="w-full h-full"></div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const mapContainer = ref(null)
+
+onMounted(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiaG91c2VvZnNkb3QiLCJhIjoiUFVIX1VLayJ9.2NVTq3ewktsyDNM8lBRtxQ'
+
+    new mapboxgl.Map({
+        container: mapContainer.value,
+        // container: 'mapContainer',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-74.5, 40],
+        zoom: 9
+    })
+})
+</script>
