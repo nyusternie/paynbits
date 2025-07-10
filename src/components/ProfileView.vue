468fe0a168c0e51301426777634b232c85c4bf72
Shomari <nyusternie@users.noreply.github.com>
2025-07-10 03:03:47 -0400

    Add profile content.

--- src/components/ProfileView.vue
+++ src/components/ProfileView.vue
@@ -1,7 +1,13 @@
 <template>
-    <h1>
-        PROFILE PAGE HERE
-    </h1>
+    <main>
+        <h1>
+            PROFILE PAGE HERE
+        </h1>
+        
+        <p>
+            lorem ipsum etc etc...
+        </p>
+    </main>
 </template>
 
 <script setup lang="ts">
